d5ac9cbff56656217f6a8815ef37f54aaac46cdf
Janeeeeeee1 <40857429+Janeeeeeee1@users.noreply.github.com>
2018-10-17 00:56:10 -0500

    update

---
+++ 网页换肤/index.js
@@ -0,0 +1,16 @@
+$(function(){
+    let $li =$("#skin li");
+    $li.click(function(){
+        switchSkin( this.id );
+    });
+    let cookie_skin = $.cookie( "MyCssSkin");
+    if (cookie_skin) {
+        switchSkin( cookie_skin );
+    }
+});
+function switchSkin(skinName){
+    $("#"+skinName).addClass("selected")                 //当前<li>元素选中
+        .siblings().removeClass("selected");  //去掉其它同辈<li>元素的选中
+    $("#cssfile").attr("href","css/"+ skinName +".css"); //设置不同皮肤
+    $.cookie( "MyCssSkin" ,  skinName , { path: '/', expires: 10 });
+}
